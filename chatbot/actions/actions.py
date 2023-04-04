99a015134dced461d0f09f910821bad0def16ea2
carla <carlasofiamendes@outlook.com>
2023-04-04 01:30:08 +0100

    Chatbot connect to Got API

--- chatbot/actions/actions.py
+++ chatbot/actions/actions.py
@@ -1,17 +1,21 @@
-# This files contains your custom actions which can be used to run
-# custom Python code.
-#
-# See this guide on how to implement these action:
-# https://rasa.com/docs/rasa/custom-actions
-
-
-# This is a simple example for a custom action which utters "Hello World!"
-
-from typing import Any, Text, Dict, List
-
-from rasa_sdk import Action, Tracker
-from rasa_sdk.executor import CollectingDispatcher
+import os
+import requests
+from dotenv import load_dotenv
 from rasa_sdk.events import SlotSet
+from rasa_sdk import Action, Tracker
+from typing import Any, Text, Dict, List
+from rasa_sdk.executor import CollectingDispatcher
+
+load_dotenv()
+SERVER = str(os.getenv('SERVER'))
+
+def request(url):
+    payload={}
+    headers = {}
+    r = requests.request("GET", url, headers=headers, data=payload)
+    if r.status_code != 200:
+        return "[Error]["+str(r.status_code)+"] - "  + str(r.json())
+    return str(r.json())
 
 class ActionExtractCharacter(Action):
     def name(self) -> Text:
@@ -21,9 +25,10 @@ class ActionExtractCharacter(Action):
             tracker: Tracker,
             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
         character = tracker.latest_message['entities'][0]['value']
-        SlotSet("character", character.replace(" ","%20"))
+        SlotSet("character", character)
+        characterData = request(SERVER+"/characters?name="+character.replace(" ","%20"))
         dispatcher.utter_message(f"Here is what I know about {character}")
-    
+        dispatcher.utter_message(characterData)
 class ActionExtractHouse(Action):
     def name(self) -> Text:
         return "action_extract_house"
@@ -32,5 +37,7 @@ class ActionExtractHouse(Action):
             tracker: Tracker,
             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
         house = tracker.latest_message['entities'][0]['value']
-        SlotSet("house", house.replace(" ","%20"))
+        SlotSet("house", house)
+        houseData = request(SERVER+"/houses?name=House%20"+house.replace(" ","%20"))
         dispatcher.utter_message(f"Here is what I know about House {house}")
+        dispatcher.utter_message(houseData)
